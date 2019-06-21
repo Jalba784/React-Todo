ae99417be8f883b5e932dff52d800fc7c00fab54
josepaniagua <jpanjr@gmail.com>
2019-06-20 22:17:48 -0700

    todolist

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,21 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo.js';
+
+const TodoList = props => {
+    // console.log("TodoList Props:", props);
+    return (
+        <div className='shopping-list'>
+            {props.todoItems.map(item => {
+                return (
+                    <Item
+                        groceryItem={item}
+                        toggleItem={props.toggleItem}
+                        key={item.id}
+                    />
+                );
+            })}
+        </div>
+    );
+};
